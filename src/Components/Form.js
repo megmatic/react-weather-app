62aaadef3ae59ea469a950ddea44e7006c553b9f
Austin King <austintriesemail@gmail.com>
2022-02-17 09:56:58 -0600

    Add useEffect

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import LocationService from "../services/LocationService";
 import WeatherService from "../services/WeatherService";
 
@@ -7,40 +7,33 @@ function Form() {
   const [lat, setLat] = useState()
   const [lng, setLng] = useState()
   const [location, setLocation] = useState("");
-  const [locationInfo, setLocationInfo] = useState('')
+  const [locationInfo, setLocationInfo] = useState([]);
 
+  
+  async function getForcast() {
+    const res = await WeatherService.getForcast(lat, lng)
+    setLocationInfo(res.data)
+  }
+  
   const getGeocode = async () => {
     const res = await LocationService.getGeocode(location)
-    const latitude = res.data.results[0].geometry.location.lat
-    const longitude = res.data.results[0].geometry.location.lng
-    setLat(latitude)
-    setLng(longitude)
-    console.log(latitude, longitude, lat, lng)
+    setLat(res.data.results[0].geometry.location.lat)
+    setLng(res.data.results[0].geometry.location.lng)
   }
-
-  const getForcast = async () => {
-    const res = await WeatherService.getForcast(lat, lng)
-    console.log(res.data)
-  }
+  
+  useEffect(() => {
+    getForcast();
+  },[lat,lng])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     await getGeocode()
     await getForcast()
+    console.log(locationInfo.city.name)
   }
 
   return (
     <div className="search">
-      {/* <form onSubmit={handleSubmit}>
-        <input
-          className="search"
-          type="text"
-          placeholder="Add Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-      </form> */}
-
       <div className="search-box">
         <button className="btn-search"><i className="fas fa-search"></i></button>
         <form onSubmit={handleSubmit}>
