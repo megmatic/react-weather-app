4a6281b69a4090e674aa9420c183c845492405d1
Meg <helloiammegg@gmail.com>
2022-02-17 15:18:18 -0600

    remove commented context version

--- src/Contexts/WeatherDataContext.js
+++ src/Contexts/WeatherDataContext.js
@@ -11,28 +11,3 @@ export function WeatherDataProvider({ children }) {
     </WeatherDataContext.Provider>
   )
 }
-
-// import { useState, useContext, createContext } from "react"
-
-// const WeatherDataContext = createContext()
-// const WeatherDataUpdateContext = createContext()
-
-// export function useWeatherData() {
-//   return useContext(WeatherDataContext)
-// }
-
-// export function useWeatherDataUpdate() {
-//   return useContext(WeatherDataUpdateContext)
-// }
-
-// export function WeatherDataProvider({ children }) {
-//   const [weatherData, setWeatherData] = useState()
-
-//   return (
-//     <WeatherDataContext.Provider value={weatherData}>
-//       <WeatherDataUpdateContext.Provider value={setWeatherData}>
-//         {children}
-//       </WeatherDataUpdateContext.Provider>
-//     </WeatherDataContext.Provider>
-//   )
-// }
